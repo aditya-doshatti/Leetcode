75cb36a628b6670a738eb55059b91504c006c02e
Aditya Doshatti <aditya.doshatti@sjsu.edu>
2020-04-02 19:09:16 -0700

    Added solution for 841

---
+++ keys_and_rooms_841.py
@@ -0,0 +1,38 @@
+'''
+841. Keys and Rooms
+Medium
+
+There are N rooms and you start in room 0.  Each room has a distinct number in 0, 1, 2, ..., N-1, and each room may have some keys to access the next room. 
+
+Formally, each room i has a list of keys rooms[i], and each key rooms[i][j] is an integer in [0, 1, ..., N-1] where N = rooms.length.  A key rooms[i][j] = v opens the room with number v.
+
+Initially, all the rooms start locked (except for room 0). 
+
+You can walk back and forth between rooms freely.
+
+Return true if and only if you can enter every room.
+
+Example 1:
+
+Input: [[1],[2],[3],[]]
+Output: true
+Explanation:  
+We start in room 0, and pick up key 1.
+We then go to room 1, and pick up key 2.
+We then go to room 2, and pick up key 3.
+We then go to room 3.  Since we were able to go to every room, we return true.
+
+https://leetcode.com/problems/keys-and-rooms/
+'''
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visited = set()
+        queue = [0]
+        while queue:
+            curr = queue.pop()
+            if curr not in visited:
+                visited.add(curr)
+                queue += rooms[curr]
+                if len(visited) == len(rooms):
+                    return True
+        return False
