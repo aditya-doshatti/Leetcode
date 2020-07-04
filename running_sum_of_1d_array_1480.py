0257e5c8f4313d93ad54467cd119ffd0a0ec1ee3
Aditya Doshatti <aditya.doshatti@sjsu.edu>
2020-07-03 17:24:19 -0700

    Added solution for 1480

---
+++ running_sum_of_1d_array_1480.py
@@ -0,0 +1,27 @@
+'''
+1480. Running Sum of 1d Array
+Easy
+
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+https://leetcode.com/problems/running-sum-of-1d-array/
+'''
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+        return nums
+        # retVal = []
+        # sumVal = 0
+        # for i in nums:
+        #     sumVal += i
+        #     retVal.append(sumVal)
+        # return retVal
