c32510a911c648b0a146c15343977db0af0d00fe
Aditya Doshatti <aditya.doshatti@sjsu.edu>
2020-07-12 19:34:44 -0700

    Added solution for 190

---
+++ reverse_bits_190.py
@@ -0,0 +1,23 @@
+'''
+190. Reverse Bits
+Easy
+
+Reverse bits of a given 32 bits unsigned integer.
+
+ 
+
+Example 1:
+
+Input: 00000010100101000001111010011100
+Output: 00111001011110000010100101000000
+Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+
+https://leetcode.com/problems/reverse-bits/
+'''
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        retVal = 0
+        for i in range(32):
+            retVal = (retVal << 1) + (n & 1)
+            n >>= 1
+        return retVal
