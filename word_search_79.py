70798080ac9de55571ab27a91db2d41e1dd0e4cb
Aditya Doshatti <aditya.doshatti@sjsu.edu>
2020-07-21 18:18:29 -0700

    Added solution for 79

---
+++ word_search_79.py
@@ -0,0 +1,49 @@
+'''
+79. Word Search
+Medium
+
+Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+Example:
+
+board =
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+
+Given word = "ABCCED", return true.
+Given word = "SEE", return true.
+Given word = "ABCB", return false.
+
+https://leetcode.com/problems/word-search/
+'''
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        visited = [[False for j in range(len(board[0]))] for i in range(len(board))]
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.dfs(board, word, 0, i, j, visited):
+                    return True
+
+        return False
+
+    def dfs(self, board, word, cur, i, j, visited):
+        if cur == len(word):
+            return True
+
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or visited[i][j] or board[i][j] != word[cur]:
+            return False
+
+        visited[i][j] = True
+        result = self.dfs(board, word, cur + 1, i + 1, j, visited) or\
+                 self.dfs(board, word, cur + 1, i - 1, j, visited) or\
+                 self.dfs(board, word, cur + 1, i, j + 1, visited) or\
+                 self.dfs(board, word, cur + 1, i, j - 1, visited)
+        visited[i][j] = False
+
+        return result
