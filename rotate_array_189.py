0a4ac18e65f2a27268ce187d9c933a3a55825a6c
Aditya Doshatti <aditya.doshatti@sjsu.edu>
2020-10-15 17:33:02 -0700

    Added solution for 189

---
+++ rotate_array_189.py
@@ -0,0 +1,30 @@
+'''
+189. Rotate Array
+Medium
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Follow up:
+
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+https://leetcode.com/problems/rotate-array/
+'''
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums[:] = (nums[len(nums)-k:] + nums[:len(nums)-k])
