2593b017f073919985987edff8921884218db573
Aditya Doshatti <aditya.doshatti@sjsu.edu>
2020-11-08 20:06:46 -0800

    Added solution for 563

---
+++ binary_tree_tilt_563.py
@@ -0,0 +1,46 @@
+'''
+563. Binary Tree Tilt
+Easy
+
+Given the root of a binary tree, return the sum of every tree node's tilt.
+
+The tilt of a tree node is the absolute difference between the sum of all left subtree node values and all right subtree node values. If a node does not have a left child, then the sum of the left subtree node values is treated as 0. The rule is similar if there the node does not have a right child.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3]
+Output: 1
+Explanation: 
+Tilt of node 2 : |0-0| = 0 (no children)
+Tilt of node 3 : |0-0| = 0 (no children)
+Tile of node 1 : |2-3| = 1 (left subtree is just left child, so sum is 2; right subtree is just right child, so sum is 3)
+Sum of every tilt : 0 + 0 + 1 = 1
+
+https://leetcode.com/problems/binary-tree-tilt/
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    retVal = 0
+    def findTilt(self, root: TreeNode) -> int:
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.findTiltUtil(root)
+        return self.retVal
+
+    def findTiltUtil(self, root):
+        if root == None:
+            return 0
+        lef = self.findTiltUtil(root.left)
+        rig = self.findTiltUtil(root.right)
+        self.retVal += abs(lef-rig)
+        return (lef+rig) + root.val
