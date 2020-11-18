a5e95a505c18aa74b166bb88ae0577c9cc4bc874
Aditya Doshatti <aditya.doshatti@sjsu.edu>
2020-11-17 18:52:41 -0800

    Added solution for 858

---
+++ mirror_reflection_858.py
@@ -0,0 +1,33 @@
+'''
+858. Mirror Reflection
+Medium
+
+There is a special square room with mirrors on each of the four walls.  Except for the southwest corner, there are receptors on each of the remaining corners, numbered 0, 1, and 2.
+
+The square room has walls of length p, and a laser ray from the southwest corner first meets the east wall at a distance q from the 0th receptor.
+
+Return the number of the receptor that the ray meets first.  (It is guaranteed that the ray will meet a receptor eventually.)
+
+ 
+
+Example 1:
+
+Input: p = 2, q = 1
+Output: 2
+Explanation: The ray meets receptor 2 the first time it gets reflected back to the left wall.
+
+https://leetcode.com/problems/mirror-reflection/
+'''
+class Solution:
+    def mirrorReflection(self, p: int, q: int) -> int: 
+        for i in range(1, min(p,q)+1): 
+            if((p % i == 0) and (q % i == 0)): 
+                gcd = i 
+        p = (p/gcd) % 2
+        q = (q/gcd) % 2
+        if p and q:
+            return 1
+        elif p:
+            return 0
+        else:
+            return 2
