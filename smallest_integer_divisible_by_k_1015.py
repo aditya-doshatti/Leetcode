748f852fb82e7e5dc2466651ef4069a25992a063
Aditya Doshatti <aditya.doshatti@sjsu.edu>
2020-11-25 16:19:31 -0800

    Added solution for 1015

---
+++ smallest_integer_divisible_by_k_1015.py
@@ -0,0 +1,28 @@
+'''
+1015. Smallest Integer Divisible by K
+Medium
+
+Given a positive integer K, you need to find the length of the smallest positive integer N such that N is divisible by K, and N only contains the digit 1.
+
+Return the length of N. If there is no such N, return -1.
+
+Note: N may not fit in a 64-bit signed integer.
+
+ 
+
+Example 1:
+
+Input: K = 1
+Output: 1
+Explanation: The smallest answer is N = 1, which has length 1.
+
+https://leetcode.com/problems/smallest-integer-divisible-by-k/
+'''
+class Solution:
+    def smallestRepunitDivByK(self, K: int) -> int:
+        remainder = 0
+        for length_N in range(1,K+1):
+            remainder = (remainder*10+1) % K
+            if remainder == 0:
+                return length_N
+        return -1
