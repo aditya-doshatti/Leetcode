1047fc367099649c9b75f9b182c5dc7fbb5776f3
Aditya Doshatti <aditya.doshatti@sjsu.edu>
2021-04-16 18:46:50 -0700

    Added solution for 1209

---
+++ remove_all_adjacent_duplicates_in_string_ii_1209.py
@@ -0,0 +1,32 @@
+'''
+1209. Remove All Adjacent Duplicates in String II
+Medium
+
+You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
+
+We repeatedly make k duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "abcd", k = 2
+Output: "abcd"
+Explanation: There's nothing to delete.
+
+https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/
+'''
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = [['*', 1]]
+        for i in range(len(s)):
+            if s[i] == stack[-1][0]:
+                stack[-1][1] += 1
+            else:
+                stack.append([s[i], 1])
+            if stack[-1][1] == k:
+                stack.pop()
+        retVal = ''.join(i*j for i, j in stack)
+        return retVal[1:]
