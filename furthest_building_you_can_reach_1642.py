17f57317c99cedfaa0553e5eceaf31a7250a4cd2
Aditya Doshatti <aditya.doshatti@sjsu.edu>
2021-04-26 21:31:28 -0700

    Added solution for 1642

---
+++ furthest_building_you_can_reach_1642.py
@@ -0,0 +1,46 @@
+'''
+1642. Furthest Building You Can Reach
+Medium
+
+You are given an integer array heights representing the heights of buildings, some bricks, and some ladders.
+
+You start your journey from building 0 and move to the next building by possibly using bricks or ladders.
+
+While moving from building i to building i+1 (0-indexed),
+
+If the current building's height is greater than or equal to the next building's height, you do not need a ladder or bricks.
+If the current building's height is less than the next building's height, you can either use one ladder or (h[i+1] - h[i]) bricks.
+Return the furthest building index (0-indexed) you can reach if you use the given ladders and bricks optimally.
+
+ 
+
+Example 1:
+
+
+Input: heights = [4,2,7,6,9,14,12], bricks = 5, ladders = 1
+Output: 4
+Explanation: Starting at building 0, you can follow these steps:
+- Go to building 1 without using ladders nor bricks since 4 >= 2.
+- Go to building 2 using 5 bricks. You must use either bricks or ladders because 2 < 7.
+- Go to building 3 without using ladders nor bricks since 7 >= 6.
+- Go to building 4 using your only ladder. You must use either bricks or ladders because 6 < 9.
+It is impossible to go beyond building 4 because you do not have any more bricks or ladders.
+
+https://leetcode.com/problems/furthest-building-you-can-reach/
+'''
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        heap = []
+        for i in range(len(heights) - 1):
+            diff = heights[i+1] - heights[i]
+            if diff > 0:
+                if ladders > 0:
+                    heappush(heap, diff)
+                    ladders -= 1
+                elif heap and diff > heap[0]:
+                    heappush(heap, diff)
+                    bricks -= heappop(heap)
+                else: bricks -= diff
+                if bricks < 0: 
+                    return i
+        return len(heights) - 1
