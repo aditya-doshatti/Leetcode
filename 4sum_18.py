5fc89ef39742888fd0a8ff46f5f20e4481c347ff
Aditya Doshatti <42703118+aditya-doshatti@users.noreply.github.com>
2021-07-16 18:00:38 -0700

    Added solution for 18

---
+++ 4sum_18.py
@@ -0,0 +1,45 @@
+'''
+18. 4Sum
+Medium
+
+Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+
+https://leetcode.com/problems/4sum/
+'''
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        def findNsum(l, r, target, N, result, retVal):
+            if r-l+1 < N or N < 2 or target < nums[l]*N or target > nums[r]*N:  # early termination
+                return
+            if N == 2: # two pointers solve sorted 2-sum problem
+                while l < r:
+                    s = nums[l] + nums[r]
+                    if s == target:
+                        retVal.append(result + [nums[l], nums[r]])
+                        l += 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+            else: # recursively reduce N
+                for i in range(l, r+1):
+                    if i == l or (i > l and nums[i-1] != nums[i]):
+                        findNsum(i+1, r, target-nums[i], N-1, result+[nums[i]], retVal) 
+        nums.sort()
+        retVal = []
+        findNsum(0, len(nums)-1, target, 4, [], retVal)
+        return retVal
