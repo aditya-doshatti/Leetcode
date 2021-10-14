7ed2abf47f1b3df836cc4d221f6a0b2962397480
Aditya Doshatti <42703118+aditya-doshatti@users.noreply.github.com>
2021-10-13 17:23:42 -0700

    Update construct_binary_search_tree_from_preorder_traversal_1008.py

--- construct_binary_search_tree_from_preorder_traversal_1008.py
+++ construct_binary_search_tree_from_preorder_traversal_1008.py
@@ -17,15 +17,6 @@ https://leetcode.com/problems/construct-binary-search-tree-from-preorder-travers
 '''
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution:
-    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
-        # Definition for a binary tree node.
-# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
