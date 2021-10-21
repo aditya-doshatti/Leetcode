e7fae56e85a6f4a2df18c7b582b0580373551826
Aditya Doshatti <42703118+aditya-doshatti@users.noreply.github.com>
2021-10-20 17:11:44 -0700

    Update insert_delete_getrandom_o1_380.py

--- insert_delete_getrandom_o1_380.py
+++ insert_delete_getrandom_o1_380.py
@@ -35,61 +35,28 @@ randomSet.getRandom();
 
 https://leetcode.com/problems/insert-delete-getrandom-o1/
 '''
+import random
 class RandomizedSet:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.dic = {}
-        self.lst = []
-        # self.dic = {}
-        
 
     def insert(self, val: int) -> bool:
-        """
-        Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        """
-        if val in self.dic:
+        if val in self.dic.keys():
             return False
-        self.lst.append(val)
-        self.dic[val] = len(self.lst) - 1
+        self.dic[val] = 1
         return True
-    
-        # if val not in self.dic:
-        #     self.dic[val] = val
-        #     return True
-        # return False
-        
 
     def remove(self, val: int) -> bool:
-        """
-        Removes a value from the set. Returns true if the set contained the specified element.
-        """
-        if val not in self.dic:
-            return False
-        i = self.dic[val]
-        self.lst[i] = self.lst[-1]
-        self.dic[self.lst[i]] = i
-        del self.lst[-1]
-        del self.dic[val]
-        return True
-    
-        # if val in self.dic:
-        #     del(self.dic[val])
-        #     return True
-        # return False
-        
+        if val in self.dic.keys():
+            del(self.dic[val])
+            return True
+        return False
 
     def getRandom(self) -> int:
-        """
-        Get a random element from the set.
-        """
-        return random.choice(self.lst)
-    
-        # lst = list(self.dic.items())
-        # # randomNo = random.randint(0, len(lst)-1)
-        # return random.choice(lst)[0]
+        lst = list(self.dic.keys())
+        return random.choice(lst)
+
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
