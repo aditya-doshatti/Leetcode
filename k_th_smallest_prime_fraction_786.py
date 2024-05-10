fc2013c3fece8583365e4a3ca79b732da6ed4b8a
Aditya Doshatti <42703118+aditya-doshatti@users.noreply.github.com>
2024-05-10 14:44:47 -0700

    Added solution for 786

---
+++ k_th_smallest_prime_fraction_786.py
@@ -0,0 +1,34 @@
+'''
+786. K-th Smallest Prime Fraction
+
+Medium
+
+You are given a sorted integer array arr containing 1 and prime numbers, where all the integers of arr are unique. You are also given an integer k.
+
+For every i and j where 0 <= i < j < arr.length, we consider the fraction arr[i] / arr[j].
+
+Return the kth smallest fraction considered. Return your answer as an array of integers of size 2, where answer[0] == arr[i] and answer[1] == arr[j].
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,3,5], k = 3
+Output: [2,5]
+Explanation: The fractions to be considered in sorted order are:
+1/5, 1/3, 2/5, 1/2, 3/5, and 2/3.
+The third fraction is 2/5.
+
+https://leetcode.com/problems/k-th-smallest-prime-fraction/description/
+'''
+import heapq
+class Solution:
+    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+        values = []
+        for i in range(len(arr)):
+            for j in range(i+1, len(arr)):
+                heapq.heappush(values, (arr[i]/arr[j], arr[i], arr[j]))
+        while k:
+            val = heapq.heappop(values)
+            k -= 1
+        return [val[1], val[2]]
