a909107f46e5452e564d99bb6da5e996d37b19e9
Aditya Doshatti <42703118+aditya-doshatti@users.noreply.github.com>
2024-06-16 17:30:30 -0700

    Update sum_of_square_numbers_633.py

--- sum_of_square_numbers_633.py
+++ sum_of_square_numbers_633.py
@@ -14,7 +14,31 @@ Explanation: 1 * 1 + 2 * 2 = 5
 https://leetcode.com/problems/sum-of-square-numbers/
 '''
 class Solution:
-    def judgeSquareSum(self, c: int) -> bool:
+    def judgeSquareSum(self, c: int) -> bool
+        i , maxVal = 0, int(math.sqrt(c))
+        while i <= maxVal:
+            sumVal = i ** 2 + maxVal ** 2
+            if sumVal == c:
+                return True
+            elif sumVal > c:
+                maxVal -= 1
+            else:
+                i += 1
+        return False
+        '''
+        if c == 0:
+            return True
+        maxVal = c**0.5
+        maxVal = maxVal//1
+        while maxVal > 0:
+            diff = c - (maxVal ** 2)
+            # print(diff, diff ** 0.5)
+            if (diff ** 0.5) % 1 == 0:
+                return True
+            maxVal -= 1
+        return False
+        '''
+        '''
         sqrVal = set()
         maxVal = int(math.sqrt(c))
         for i in range(maxVal + 1):
@@ -23,3 +47,4 @@ class Solution:
             if c - n in sqrVal:
                 return True
         return False
+        '''
