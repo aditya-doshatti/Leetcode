ce88f1587e078ba8c3d92be7c004ddd4c485a871
Aditya Doshatti <42703118+aditya-doshatti@users.noreply.github.com>
2024-07-17 16:26:08 -0700

    Added solution for 1110

---
+++ delete_nodes_and_return_forest_1110.py
@@ -0,0 +1,42 @@
+'''
+1110. Delete Nodes And Return Forest
+
+Medium
+
+Given the root of a binary tree, each node in the tree has a distinct value.
+
+After deleting all nodes with a value in to_delete, we are left with a forest (a disjoint union of trees).
+
+Return the roots of the trees in the remaining forest. You may return the result in any order.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5,6,7], to_delete = [3,5]
+Output: [[1,2,null,4],[6],[7]]
+
+https://leetcode.com/problems/delete-nodes-and-return-forest/description
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        s = set(to_delete)
+        retVal = []
+        def dfs(root, flag):
+            if not root: 
+                return None
+            toDelete = (root.val in s)
+            root.left = dfs(root.left, toDelete)
+            root.right = dfs(root.right, toDelete)
+            if not toDelete and flag: 
+                retVal.append(root)
+            return None if toDelete else root
+        dfs(root, True)
+        return retVal
