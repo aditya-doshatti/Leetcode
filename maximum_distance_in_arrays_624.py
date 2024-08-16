cb6c4600380cf114fa46222f69b316bc1df9ae27
Aditya Doshatti <42703118+aditya-doshatti@users.noreply.github.com>
2024-08-16 14:57:43 -0700

    Added solution for 624

---
+++ maximum_distance_in_arrays_624.py
@@ -0,0 +1,39 @@
+'''
+624. Maximum Distance in Arrays
+
+Medium
+
+You are given m arrays, where each array is sorted in ascending order.
+
+You can pick up two integers from two different arrays (each array picks one) and calculate the distance. We define the distance between two integers a and b to be their absolute difference |a - b|.
+
+Return the maximum distance.
+
+ 
+
+Example 1:
+
+Input: arrays = [[1,2,3],[4,5],[1,2,3]]
+Output: 4
+Explanation: One way to reach the maximum distance 4 is to pick 1 in the first or third array and pick 5 in the second array.
+
+https://leetcode.com/problems/maximum-distance-in-arrays/description
+'''
+class Solution:
+    def maxDistance(self, arrays: List[List[int]]) -> int:
+        smallest = arrays[0][0]
+        biggest = arrays[0][-1]
+        max_distance = 0
+
+        for i in range(1, len(arrays)):
+            arr = arrays[i]
+            max_distance = max(max_distance, abs(arr[-1] - smallest), abs(biggest - arr[0]))
+            smallest = min(smallest, arr[0])
+            biggest = max(biggest, arr[-1])
+
+        return max_distance
+        '''
+        Wrong answer with negative numbers
+        arrays.sort()
+        return abs(arrays[0][0] - arrays[-1][-1])
+        '''
