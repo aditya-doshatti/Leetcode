c64fad2488ced0a87e6cb45057b6059204445efd
Aditya Doshatti <42703118+aditya-doshatti@users.noreply.github.com>
2024-08-27 18:51:32 -0700

    Added solution for 521

---
+++ longest_uncommon_subsequence_i_521.py
@@ -0,0 +1,27 @@
+'''
+521. Longest Uncommon Subsequence I
+
+Easy
+
+Given two strings a and b, return the length of the longest uncommon subsequence between a and b. If no such uncommon subsequence exists, return -1.
+
+An uncommon subsequence between two strings is a string that is a 
+subsequence
+ of exactly one of them.
+
+ 
+
+Example 1:
+
+Input: a = "aba", b = "cdc"
+Output: 3
+Explanation: One longest uncommon subsequence is "aba" because "aba" is a subsequence of "aba" but not "cdc".
+Note that "cdc" is also a longest uncommon subsequence.
+
+https://leetcode.com/problems/longest-uncommon-subsequence-i/description/
+'''
+class Solution:
+    def findLUSlength(self, a: str, b: str) -> int:
+        if a == b:
+            return -1
+        return max(len(a), len(b))
